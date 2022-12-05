cfdb0f10d49f8a0ca65463f195c0d85636014c7f
Andrei_Novikov <novikovas1@mail.ru>
2022-12-05 16:21:36 +0300

    Лабораторные задания в занятии №2

--- Занятие2/Лабораторные_задания/task2_1/main.py
+++ Занятие2/Лабораторные_задания/task2_1/main.py
@@ -3,9 +3,11 @@ from itertools import count
 
 def task():
     num = 2 ** 0  # 1
+    yield num
     # TODO с помощью yield вернуть первое число
 
     for i in count(1, 1):
+        yield pow(2,i) # А как предпочтительнее писать? "pow(2,i)" или "i ** 2"?
         # TODO с помощью yield вернуть оставшиеся степени двойки до бесконечности
 
 
--- Занятие2/Лабораторные_задания/task3_2/main.py
+++ Занятие2/Лабораторные_задания/task3_2/main.py
@@ -1,10 +1,16 @@
 def min_len_check(fn):
+    def wrapper(str_arg):
+        if len(str_arg) < 10:
+            raise ValueError("Строка слишком короткая")
+
+        result = fn(str_arg)
     # TODO записать обертку для исходной функции
 
     return wrapper
 
 
 # TODO задекорировать функцию
+@min_len_check
 def some_func(str_arg: str):
     ...
 
