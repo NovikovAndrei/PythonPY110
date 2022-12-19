916818101b323b7a7ec5d000a5e475ba910cf4e7
Andrei_Novikov <novikovas1@mail.ru>
2022-12-19 13:07:39 +0300

    Лабораторные задания в занятии №4

--- Занятие4/Лабораторные_задания/task1_1/main.py
+++ Занятие4/Лабораторные_задания/task1_1/main.py
@@ -2,15 +2,19 @@ import json
 import re
 
 BOOKS_FILE = "books.md"
-BOOK_REGEX = ...  # TODO записать ругулярное выражения для поиска книги
-
+# TODO записать ругулярное выражения для поиска книги
+BOOK_REGEX = r"(?P<position>(?<=#\s)\d{1,2})\.\s\[(?P<book>.+?)\]\(" \
+               r"(?P<book_url>(https://.+?))\)\sby\s+(?P<author>([\w\.\&\s\']+))" \
+               r"\((?P<recommended>(\d{1,2}\.\d{1,2}\%))\srecommended\)\s" \
+               r"\!\[\]\((?P<cover_url>(https://.+?))\)\s+" \
+               r"\"(?P<description>.+?)\""
 
 def task():
     book_pattern = re.compile(BOOK_REGEX, re.DOTALL)  # флаг re.DOTALL описывает, что под символом точка может содержаться символ переноса строки
 
-    with open(BOOKS_FILE) as f:
+    with open(BOOKS_FILE, encoding='utf-8') as f:
         for book in book_pattern.finditer(f.read()):
-            print(json.dumps(book.groupdict(), indent=4))
+            print(json.dumps(book.groupdict(), indent=4, ensure_ascii=False ))
 
 
 if __name__ == '__main__':
